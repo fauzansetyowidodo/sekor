9ec915e65bdd79af7b4c381f95a427d49f35fa1f
Fauzan Setyowidodo <fauzansetyowidodo@gmail.com>
2021-08-06 13:23:33 +0700

    finishing feature section

--- src/parts/Feature.js
+++ src/parts/Feature.js
@@ -1,8 +1,68 @@
 import React from "react";
 
 export default function Feature({ data }) {
-  return (
-    <section className="container feature">
+  return data.map((category, index1) => {
+    if (index1 % 2 === 0) {
+      return (
+        <section className="container feature" key={`caategory-${index1}`}>
+          {category.items.map((item, index2) => {
+            return (
+              <div className="container align-middle">
+                <div
+                  className="row align-items-center"
+                  key={`feature-${index2}`}
+                >
+                  <div className="col-6">
+                    <p className="kecil">{item.bag}</p>
+                    <h1>{item.title}</h1>
+                    <p className="desc">{item.desc}</p>
+                  </div>
+                  <div className="col">
+                    <figure>
+                      <img src={item.imageUrl} alt="" />
+                    </figure>
+                  </div>
+                </div>
+                ;
+              </div>
+            );
+          })}
+        </section>
+      );
+    } else {
+      return (
+        <section className="container feature" key={`caategory-${index1}`}>
+          {category.items.map((item, index2) => {
+            return (
+              <div className="container align-middle">
+                <div
+                  className="row align-items-center"
+                  key={`feature-${index2}`}
+                >
+                  <div className="col">
+                    <figure>
+                      <img src={item.imageUrl} alt="" />
+                    </figure>
+                  </div>
+
+                  <div className="col-6">
+                    <p className="kecil">{item.bag}</p>
+                    <h1>{item.title}</h1>
+                    <p className="desc">{item.desc}</p>
+                  </div>
+                </div>
+                ;
+              </div>
+            );
+          })}
+        </section>
+      );
+    }
+  });
+}
+
+{
+  /* <section className="container feature">
       {data.map((item, index) => {
         return (
           <div className="container align-middle">
@@ -22,6 +82,5 @@ export default function Feature({ data }) {
           </div>
         );
       })}
-    </section>
-  );
+    </section> */
 }
