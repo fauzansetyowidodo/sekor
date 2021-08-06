23533dd8e54ff0ebc68b8a21fe998b73a2c08fa3
Fauzan Setyowidodo <fauzansetyowidodo@gmail.com>
2021-08-06 13:24:57 +0700

    Update Feature.js

--- src/parts/Feature.js
+++ src/parts/Feature.js
@@ -60,27 +60,3 @@ export default function Feature({ data }) {
     }
   });
 }
-
-{
-  /* <section className="container feature">
-      {data.map((item, index) => {
-        return (
-          <div className="container align-middle">
-            <div className="row align-items-center" key={`feature-${index}`}>
-              <div className="col-6">
-                <p className="kecil">{item.category}</p>
-                <h1>{item.title}</h1>
-                <p className="desc">{item.desc}</p>
-              </div>
-              <div className="col">
-                <figure>
-                  <img src={item.imageUrl} alt="" />
-                </figure>
-              </div>
-            </div>
-            ;
-          </div>
-        );
-      })}
-    </section> */
-}
